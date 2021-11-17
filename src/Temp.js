d89af863e0deff6171e4dfadbd4718472b685af5
Jindolph <jindolph628@gmail.com>
2021-11-17 15:01:34 +0900

    tictactoe tutorial study

---
+++ src/Temp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Temp extends Component {
+    // constructor
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
